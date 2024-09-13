c82facc5909a0c5d626d4177e62f776da827d236
DevonNutting <126836214+DevonNutting@users.noreply.github.com>
2024-09-12 17:02:22 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,9 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+    // Write your code below. Hint: you can finish it in about 3-5 lines.
+
+    // Create a var that stores the tag with ID counter-number
+    counter_number = document.getElementById('counter-number');
+
+    // Adds 1 to the number inside our tag
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
