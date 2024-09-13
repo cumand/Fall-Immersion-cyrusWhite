eefaf4b3c52e6f450e16b61748a0bb8fe0885291
cumand <cyrus.white@hgs.hiddengeniusproject.org>
2024-09-12 17:09:26 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,6 @@
 function counter() {
-// Write your code below. Hint: you can finish it in about 3-5 lines.
+    // creates a variable with a ID counter-number
+    counter_number = document.getElementById("counter-number");
+    //adds one each time the button is clicked
+    counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
