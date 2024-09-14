6bad0c88035fbe7883d7b093bd5ad36793bbf19f
DevonNutting <126836214+DevonNutting@users.noreply.github.com>
2024-09-13 17:43:29 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,9 +1,10 @@
 function counter() {
     // Write your code below. Hint: you can finish it in about 3-5 lines.
 
-    // Create a var that stores the tag with ID counter-number
+    // Create a variable called counter_number, store the tag with the id
+    // 'counter-number' inside this variable
     counter_number = document.getElementById('counter-number');
 
-    // Adds 1 to the number inside our tag
+    // Add 1 to the text of the tag
     counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
