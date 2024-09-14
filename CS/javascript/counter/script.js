ff103f2498272827ed4d946cbd990bf233c726f0
DevonNutting <126836214+DevonNutting@users.noreply.github.com>
2024-09-14 11:43:35 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,10 +1,11 @@
 function counter() {
     // Write your code below. Hint: you can finish it in about 3-5 lines.
+    // Variable = A storage container for data
+    // Data Types: Integer, Float, String, Boolean
 
-    // Create a variable called counter_number, store the tag with the id
-    // 'counter-number' inside this variable
+    // Create a variable to store the tag we want to change
     counter_number = document.getElementById('counter-number');
 
-    // Add 1 to the text of the tag
+    // Change the text of the tag we have stored in the variable above
     counter_number.innerHTML = parseInt(counter_number.innerHTML) + 1;
 }
