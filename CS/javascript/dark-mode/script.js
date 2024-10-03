12286530f3cc6d21759a6c347b7b17057b9ac758
cumand <cyrus.white@hgs.hiddengeniusproject.org>
2024-10-02 17:51:19 -0700

    script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,17 @@
 function darkMode() {
-// Your code here.
+    
+    // Get the body element
+    const body = document.body;
+
+    // Checks if the page is white
+    if (body.style.backgroundColor === 'white') {
+        // If it's white, change to dark mode
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+    } else {
+        // If it's not white, change to white
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+    }
+
 }
