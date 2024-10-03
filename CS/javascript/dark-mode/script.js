7de2c0c9d9229efab5ecc6cea18977e415ef6aa6
DevonNutting <126836214+DevonNutting@users.noreply.github.com>
2024-10-02 17:43:59 -0700

    Update script.js

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,22 @@
 function darkMode() {
-// Your code here.
+    // Variable - A storage container data
+    // Const - A keyword in JS that defines a variable that cannot change
+
+    // Store the body tag in a variable
+    const body = document.body;
+
+    // Write the code to toggle the color of the body tag black and white
+    // If the button is clicked and the screen is currently white...
+    if (body.style.backgroundColor == "white")
+    {
+        //the screen should turn black
+        body.style.backgroundColor = "black";
+        // Change the text color to white
+        body.style.color = 'white';
+    } else { // Meaning the screen is currently not white
+        // Change the screen color to white
+        body.style.backgroundColor = "white";
+        // Change the text color to black
+        body.style.color = "black";
+    }
 }
