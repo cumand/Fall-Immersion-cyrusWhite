3e146833ba316a5181eb30a896b2e5d2876335b0
cumand <cyrus.white@hgs.hiddengeniusproject.org>
2024-10-23 17:59:01 -0700

    script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,19 @@
-function eventHandling() {
-    // Your code here.
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById('my form');
+
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
+
+
+        const name =document.getElementById('name').value
+        const email =document.getElementById('email').value
+        const message =document.getElementById('message').value
+
+
+        if (name && email && message) {
+            alert(`form submitted succesfully.\nName: ${name} \nEmail: ${email} \nMessage ${message}`);
+        } else {
+            alert ('Please fill all');
+        }
+    });
+})
